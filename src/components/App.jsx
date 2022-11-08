d20fec77a6d72b77353817cc87d8d9561500e582
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2022-11-08 23:09:40 +0200

    Add actual changes to part2 of HW#3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import ImageGallery from "./ImageGallery/ImageGallery";
+import Searchbar from './Searchbar/Searchbar'
+import React, { Component } from 'react';
+import s from '../styles.module.css';
+
+export default class App extends Component {
+  state = {
+    queryValue: '',
+  }
+  getQuery = queryValue => {
+    this.setState({ queryValue });
+  }
+  render () {
+    return (
+      <div className={s.App}>
+        <>
+          <Searchbar onSubmit={this.getQuery} />
+          <ImageGallery dataInput={this.state.queryValue} />
+        </>
+      </div>
+    );
+  };
 };
